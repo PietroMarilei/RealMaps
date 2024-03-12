329d1eecc775cc37df107a767081bf9f11627a28
pietro.marilei <pietromrl0@gmail.com>
2024-03-12 20:17:53 +0100

    add front end and api calls to get daata

---
+++ front-end /src/components/DataDisplayer.vue
@@ -0,0 +1,36 @@
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            patients: [],
+        };
+    },
+    methods: {
+        getData() {
+            axios.get('http://localhost:8000/getData.php')
+                .then(response => {
+                    this.patients = response.data;
+                })
+                .catch(error => {
+                    console.error("C'è stato un errore nel recupero dei dati:", error);
+                });
+        }
+    },
+    mounted() {
+        //this.getData(); // Chiama getData quando il componente viene montato
+    }
+}
+</script>
+
+<template>
+    <h2 class="text-center">Elenco Pazienti</h2>
+    <button @click="getData"> get  Data</button>
+    <div v-for="patient in patients">
+
+            {{ patient.FirstName }}
+    </div>
+
+</template>
---
+++ front-end /src/components/FileUpload.vue
@@ -0,0 +1,53 @@
+
+<script>
+export default {
+    data() {
+        return {
+            file: null, 
+            uploadStatus: '' 
+        };
+    },
+    methods: {
+        handleFileUpload(event) {
+            this.file = event.target.files[0]; 
+        },
+        submitFile() {
+            if (!this.file) {
+                this.uploadStatus = 'Seleziona un file prima di caricarlo.';
+                return;
+            }
+
+            let formData = new FormData();
+            formData.append('csv_file', this.file); // 'csv_file' deve corrispondere al nome utilizzato nello script PHP
+
+            this.uploadStatus = 'Caricamento in corso...';
+
+            fetch('http://localhost:8000/uploadCSV.php', { 
+                method: 'POST',
+                body: formData,
+            })
+                .then(response => response.json())
+                .then(data => {
+                    this.uploadStatus = data.message; // Mostra il messaggio di risposta dall'API
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    this.uploadStatus = 'Si è verificato un errore durante il caricamento.';
+                });
+        }
+    }
+};
+</script>
+
+<template>
+    <div>
+        <h2>Carica File CSV</h2>
+        <form @submit.prevent="submitFile">
+            <input type="file" @change="handleFileUpload" />
+            <button type="submit">Carica</button>
+        </form>
+        <div v-if="uploadStatus">
+            {{ uploadStatus }}
+        </div>
+    </div>
+</template>
